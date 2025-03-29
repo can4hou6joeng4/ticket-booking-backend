2a5cfb63dc29fe7d331976e11f24ca1f07d399ce
can4hou6joeng4 <can4hou6joeng4@163.com>
2025-03-29 23:50:45 +0800

    #1 😎 Initial commit

---
+++ cmd/api/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"github.com/can4hou6joeng4/ticket-booking-project-v1/config"
+	"github.com/can4hou6joeng4/ticket-booking-project-v1/db"
+	"github.com/can4hou6joeng4/ticket-booking-project-v1/handlers"
+	"github.com/can4hou6joeng4/ticket-booking-project-v1/middlewares"
+	"github.com/can4hou6joeng4/ticket-booking-project-v1/repositories"
+	"github.com/can4hou6joeng4/ticket-booking-project-v1/services"
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New(fiber.Config{
+		AppName:      "TickBooking",
+		ServerHeader: "Fiber",
+	})
+	// Config
+	envConfig := config.NewEnvConfig()
+	db := db.Init(envConfig, db.DBMigrator)
+	// Repository
+	eventRepository := repositorys.NewEventRepository(db)
+	ticketRepository := repositorys.NewTicketRepository(db)
+	authRepository := repositorys.NewAuthRepository(db)
+	// Service
+	authService := services.NewAuthService(authRepository)
+	// Routing
+	server := app.Group("/api")
+	privateRoutes := server.Use(middlewares.AuthProtected(db))
+	// Handlers
+	handlers.NewEventHandler(server.Group("/event"), eventRepository)
+	handlers.NewTicketHandler(server.Group("/ticket"), ticketRepository)
+	handlers.NewAuthHandler(server.Group("/auth"), authService)
+
+	app.Listen(fmt.Sprintf(":" + envConfig.ServerPort))
+}
